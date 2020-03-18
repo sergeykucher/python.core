20d9d7681789c54e277fab54943429458be0188c
sergey.kucher <sergey.kucher@gmail.com>
2020-03-18 14:50:28 +0200

    Hometask 1

---
+++ 1st.py
@@ -0,0 +1,13 @@
+a = int(input ('Input a: '))
+b = int(input ('Input b: '))
+print ("a+b=",a+b,sep='')
+print ("a-b=",a-b,sep='')
+print ("a * b =",a*b)
+print ("a / b =",a/b)
+
+your_name=input('What is you name? ')
+your_age=input('How old are you? ')
+you_live=input('Where do you live? ')
+print("Hello,",your_name)
+print("Your age is",your_age)
+print("You live in",you_live)
