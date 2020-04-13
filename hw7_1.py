4ba777d22c37300e255136142730252bfa60b0e7
Sergey Kucher <sergey.kucher@gmail.com>
2020-04-14 01:18:18 +0300

    Add Homework 7 (3 files)

---
+++ hw7_1.py
@@ -0,0 +1,2 @@
+def summation(num):
+    return sum([value for value in range(1,num+1)])
---
+++ hw7_3.py
@@ -0,0 +1,5 @@
+def double_char(s):
+    s_out = ''
+    for char in s:
+        s_out += char*2
+    return s_out
