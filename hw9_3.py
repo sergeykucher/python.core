9d3517750f87768d8d4ac36f2b52baac5eb97171
Sergey Kucher <sergey.kucher@gmail.com>
2020-04-15 13:16:01 +0300

    Add Homework 9 (4 files)

---
+++ hw9_3.py
@@ -0,0 +1,10 @@
+class Human():
+    pass
+class Man(Human):
+    pass
+class Woman(Human):
+    pass
+def God():        
+    man = Man()
+    woman = Woman()
+    return [man, woman]
---
+++ hw9_4.py
@@ -0,0 +1,3 @@
+class Person():
+    def __init__(self,name,age):
+        self.info = name+"s age is "+str(age)
